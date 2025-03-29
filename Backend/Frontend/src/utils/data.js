b85a0f0f44a6fd7eb3ec24f70aeecb98336a948e
Shaswat <kaushikchoudhary33@icloud.com>
2025-03-29 22:42:23 +0530

    Update data.js

--- Backend/Frontend/src/utils/data.js
+++ Backend/Frontend/src/utils/data.js
@@ -1,6 +1,5 @@
 export const USER_API_ENDPOINT = "https://job-caix.onrender.com//api/user";
 export const JOB_API_ENDPOINT = "https://job-caix.onrender.com/api/job";
 export const APPLICATION_API_ENDPOINT =
-  "https://job-caix.onrender.com//api/application";
-export const COMPANY_API_ENDPOINT =
-  "https://job-caix.onrender.com//api/company";
+  "https://job-caix.onrender.com/api/application";
+export const COMPANY_API_ENDPOINT = "https://job-caix.onrender.com/api/company";
