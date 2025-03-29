53526bc3c78cfad5744430dc922fb747d8116d9e
Shaswatchoudhary <kaushikchoudhary33@icloud.com>
2025-03-29 14:22:39 +0530

    final

--- BACKEND/index.js
+++ BACKEND/index.js
@@ -7,6 +7,7 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js";
+import path from "path";
 
 dotenv.config({});
 const app = express();
@@ -25,13 +26,22 @@ app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 5001;
 
- 
 //api's
 
 app.use("/api/user", userRoute);
 app.use("/api/company", companyRoute);
 app.use("/api/job", jobRoute);
 app.use("/api/application", applicationRoute);
+
+//--------------Code for development--------------------------------
+
+if (process.env.NODE_ENV === "production") {
+  const dirpath = path.resolve();
+  app.use(express.static("./Frontend/dist"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(dirpath, "./Frontend/dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
